a12c6d6b8287ea9938064c5cda8fb2c729851fa8
eungbin <programmer97@naver.com>
2023-07-26 22:04:22 +0900

    Add react-router-dom

---
+++ src/pages/User/Login.jsx
@@ -0,0 +1,17 @@
+import React, {  } from 'react';
+import styled from 'styled-components';
+
+export default function Login() {
+  return (
+    <Wrapper>
+      <span>Login Page</span>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
