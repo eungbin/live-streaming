d9e5f7a45372f7265acb39aaab37383a62dce497
eungbin <programmer97@naver.com>
2023-07-30 16:01:48 +0900

    Add Login-UI

--- src/pages/User/Login.jsx
+++ src/pages/User/Login.jsx
@@ -4,7 +4,18 @@ import styled from 'styled-components';
 export default function Login() {
   return (
     <Wrapper>
-      <span>Login Page</span>
+      <LoginBox>
+        <LoginBoxHeader>L O G I N</LoginBoxHeader>
+        <LoginInputContainer>
+          <LoginBoxInner>ID</LoginBoxInner>
+          <LoginBoxInput type='text' id='id' />
+        </LoginInputContainer>
+        <LoginInputContainer>
+          <LoginBoxInner>PASSWORD</LoginBoxInner>
+          <LoginBoxInput type='password' id='password' />
+        </LoginInputContainer>
+        <LoginButton>Click for login</LoginButton>
+      </LoginBox>
     </Wrapper>
   );
 }
@@ -14,4 +25,56 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const LoginBox = styled.div`
+  min-width: 360px;
+  background-color: #191F2B;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  padding: 16px 24px;
+  box-shadow: 0px 0px 20px 5px #191F2B;
+`;
+
+const LoginBoxHeader = styled.header`
+  font-size: 1.5rem;
+  color: white;
+  text-shadow: 0px 0px 15px white;
+  margin-bottom: 24px;
+`;
+
+const LoginInputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 12px;
+`;
+const LoginBoxInner = styled.span`
+  font-size: 1rem;
+  color: white;
+  text-shadow: 0px 0px 10px white;
+  flex-grow: 1;
+  width: 90px;
+  text-align: left;
+`;
+const LoginBoxInput = styled.input`
+  min-width: 160px;
+  min-height: 20px;
+  padding: 4px;
+  flex-grow: 2;
+  box-shadow: 0px 0px 5px 1px white;
+  border: 0px;
+`;
+
+const LoginButton = styled.button`
+  padding-top: 12px;
+  padding-bottom: 12px;
+  margin-top: 12px;
+  margin-bottom: 4px;
+  background-color: #4A5262;
+  border: 0px;
+  color: white;
+  box-shadow: 0px 0px 5px 1px #4A5262;
+  border-radius: 10px;
+  cursor: pointer;
 `;
